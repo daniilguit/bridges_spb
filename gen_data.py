8bf9a605a881f2d5c871bb37d5f93fa1371543eb
Daniil Gitelson <daniil.guit@gmail.com>
2013-05-14 14:03:39 +0400

    Updated timetable 2013

--- gen_data.py
+++ gen_data.py
@@ -4,6 +4,38 @@
 import codecs
 import urllib
 
+names = {
+    u'Александра Невского' : 'R.string.bn_AleksandraNevskogo',
+    u'Биржевой' : 'R.string.bn_Birzhevoj',
+    u'Благовещенский' : 'R.string.bn_Blagovewenskij',
+    u'Большеохтинский' : 'R.string.bn_Bolsheohtinskij',
+    u'' : 'R.string.bn_Dvorcovyj',
+    u'Финляндский' : 'R.string.bn_Finljandskij',
+    u'' : 'R.string.bn_Grenaderskij',
+    u'' : 'R.string.bn_Kantemirovskij',
+    u'Литейный' : 'R.string.bn_Litejnyj',
+    u'' : 'R.string.bn_Sampsonievskij',
+    u'Троицкий' : 'R.string.bn_Troickij',
+    u'Тучков' : 'R.string.bn_Tuchkov',
+    u'Володарский' : 'R.string.bn_Volodarskij',
+}
+
+coords = {
+    'R.string.bn_AleksandraNevskogo' : 'LocationUtil.location(30.399447, 59.926377)',
+    'R.string.bn_Birzhevoj' : 'LocationUtil.location(30.303310, 59.945143)',
+    'R.string.bn_Blagovewenskij' : 'LocationUtil.location(30.288281, 59.936115)',
+    'R.string.bn_Bolsheohtinskij' : 'LocationUtil.location(30.398037, 59.942642)',
+    'R.string.bn_Dvorcovyj' : 'LocationUtil.location(30.309508, 59.940177)',
+    'R.string.bn_Finljandskij' : 'LocationUtil.location(30.404631, 59.913965)',
+    'R.string.bn_Grenaderskij' : 'LocationUtil.location(30.332595, 59.967516)',
+    'R.string.bn_Kantemirovskij' : 'LocationUtil.location(30.320405, 59.977733)',
+    'R.string.bn_Litejnyj' : 'LocationUtil.location(30.349932, 59.953511)',
+    'R.string.bn_Sampsonievskij' : 'LocationUtil.location(30.339772, 59.957980)',
+    'R.string.bn_Troickij' : 'LocationUtil.location(30.327465, 59.948762)',
+    'R.string.bn_Tuchkov' : 'LocationUtil.location(30.284454, 59.948131)',
+    'R.string.bn_Volodarskij' : 'LocationUtil.location(30.450912, 59.877023)'
+}
+
 class Bridge:
     pass
 lines = codecs.open('timetable.txt', 'r', 'utf-8').readlines()
@@ -11,33 +43,27 @@ bridges = []
 bridge = None
 for line in lines:
     splitted_line = line.split('\t')
-    if splitted_line[0] == '2':
-        bridge.timetable +=  [((splitted_line[1], splitted_line[3]))]
+    if splitted_line[1] == '2':
+        bridge.timetable += [((splitted_line[2], splitted_line[5]))]
     else:
         bridge = Bridge()
         bridges.append(bridge)
         bridge.name = splitted_line[0]
-        bridge.timetable = [((splitted_line[2], splitted_line[4]))]
+        bridge.timetable = [((splitted_line[2], splitted_line[5]))]
 
 def format_time_java(time):
     time = time.split(':')
     return u'new Time(%s, %s)' % (time[0], time[1] if time[1][0] != '0' else time[1][1])
 
 def format_timetable_java(timetable):
-    return u'Arrays.asList(' + u','.join(map(lambda x: u'new Bridge.ClosedInterval(%s, %s)' % (format_time_java(x[0]), format_time_java(x[1])), timetable)) + u')'
+    return u'Arrays.asList(' + u','.join(map(lambda x: u'new BridgeDescription.ClosedInterval(%s, %s)' % (format_time_java(x[0]), format_time_java(x[1])), timetable)) + u')'
 
 def get_location(bridge_name):
-    url = u'http://geocode-maps.yandex.ru/1.x/?' + urllib.urlencode({'geocode':(bridge_name + u' мост петербург').encode('utf-8')})
-    for line in urllib.urlopen(url):
-        line = line.strip()
-        if line.startswith('<pos>'):
-            line = line[5:-6]
-            return line.split(' ')
+    return coords[names[bridge_name]]
 
 out = codecs.open('timetable.java', 'w', 'utf-8')
 for bridge in bridges:
-    location = get_location(bridge.name)
-    line = (u'new Bridge("{0}", location({1}, {2}), {3})').format(bridge.name, location[0], location[1], format_timetable_java(bridge.timetable))
+    line = (u'new BridgeDescription({0}, {1}, {2})').format(names[bridge.name], get_location(bridge.name), format_timetable_java(bridge.timetable))
     out.write(line)
     out.write(',\n')
 out.close()
--- src/com/ad/bridges_spb/core/BridgesDescriptions.java
+++ src/com/ad/bridges_spb/core/BridgesDescriptions.java
@@ -14,18 +14,14 @@ import java.util.Arrays;
  */
 public class BridgesDescriptions {
     public static final BridgeDescription[] BRIDGES = new BridgeDescription[]{
-            new BridgeDescription(R.string.bn_AleksandraNevskogo, LocationUtil.location(30.399447, 59.926377), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(2, 20), new Time(5, 5)))),
-            new BridgeDescription(R.string.bn_Birzhevoj, LocationUtil.location(30.303310, 59.945143), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(2, 0), new Time(4, 40)))),
-            new BridgeDescription(R.string.bn_Blagovewenskij, LocationUtil.location(30.288281, 59.936115), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(1, 25), new Time(2, 40)), new BridgeDescription.ClosedInterval(new Time(3, 10), new Time(4, 55)))),
-            new BridgeDescription(R.string.bn_Bolsheohtinskij, LocationUtil.location(30.398037, 59.942642), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(2, 0), new Time(4, 55)))),
-            new BridgeDescription(R.string.bn_Dvorcovyj, LocationUtil.location(30.309508, 59.940177), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(1, 5), new Time(4, 45)))),
-            new BridgeDescription(R.string.bn_Finljandskij, LocationUtil.location(30.404631, 59.913965), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(2, 20), new Time(5, 10)))),
-            new BridgeDescription(R.string.bn_Grenaderskij, LocationUtil.location(30.332595, 59.967516), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(2, 45), new Time(3, 40)), new BridgeDescription.ClosedInterval(new Time(4, 20), new Time(4, 45)))),
-            new BridgeDescription(R.string.bn_Kantemirovskij, LocationUtil.location(30.320405, 59.977733), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(2, 45), new Time(3, 40)), new BridgeDescription.ClosedInterval(new Time(4, 20), new Time(4, 45)))),
-            new BridgeDescription(R.string.bn_Litejnyj, LocationUtil.location(30.349932, 59.953511), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(1, 40), new Time(4, 40)))),
-            new BridgeDescription(R.string.bn_Sampsonievskij, LocationUtil.location(30.339772, 59.957980), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(2, 10), new Time(2, 40)), new BridgeDescription.ClosedInterval(new Time(3, 20), new Time(4, 20)))),
-            new BridgeDescription(R.string.bn_Troickij, LocationUtil.location(30.327465, 59.948762), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(1, 35), new Time(4, 45)))),
-            new BridgeDescription(R.string.bn_Tuchkov, LocationUtil.location(30.284454, 59.948131), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(2, 0), new Time(2, 50)), new BridgeDescription.ClosedInterval(new Time(3, 35), new Time(4, 50)))),
-            new BridgeDescription(R.string.bn_Volodarskij, LocationUtil.location(30.450912, 59.877023), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(2, 0), new Time(3, 40)), new BridgeDescription.ClosedInterval(new Time(4, 15), new Time(5, 40)))),
+            new BridgeDescription(R.string.bn_Volodarskij, LocationUtil.location(30.450912, 59.877023), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(2, 0), new Time(3, 45)), new BridgeDescription.ClosedInterval(new Time(4, 15), new Time(5, 45)))),
+            new BridgeDescription(R.string.bn_Finljandskij, LocationUtil.location(30.404631, 59.913965), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(2, 20), new Time(5, 30)))),
+            new BridgeDescription(R.string.bn_AleksandraNevskogo, LocationUtil.location(30.399447, 59.926377), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(2, 20), new Time(5, 10)))),
+            new BridgeDescription(R.string.bn_Bolsheohtinskij, LocationUtil.location(30.398037, 59.942642), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(1, 40), new Time(5, 0)))),
+            new BridgeDescription(R.string.bn_Litejnyj, LocationUtil.location(30.349932, 59.953511), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(1, 20), new Time(5, 0)))),
+            new BridgeDescription(R.string.bn_Troickij, LocationUtil.location(30.327465, 59.948762), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(1, 15), new Time(5, 0)))),
+            new BridgeDescription(R.string.bn_Blagovewenskij, LocationUtil.location(30.288281, 59.936115), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(1, 25), new Time(2, 45)), new BridgeDescription.ClosedInterval(new Time(3, 10), new Time(5, 0)))),
+            new BridgeDescription(R.string.bn_Birzhevoj, LocationUtil.location(30.303310, 59.945143), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(1, 5), new Time(5, 10)))),
+            new BridgeDescription(R.string.bn_Tuchkov, LocationUtil.location(30.284454, 59.948131), Arrays.asList(new BridgeDescription.ClosedInterval(new Time(1, 5), new Time(2, 35)), new BridgeDescription.ClosedInterval(new Time(3, 5), new Time(5, 15)))),
     };
 }
